72e27addc829647c4b65df111426af8a222054c1
Alon W <alws34@gmail.com>
2022-07-10 13:25:00 +0300

    Initial commit

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ImageMetaEditor.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(384, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(32, 31);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(12, 12);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(366, 31);
+            this.textBoxPath.TabIndex = 1;
+            this.textBoxPath.TextChanged += new System.EventHandler(this.textBoxPath_TextChanged);
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(12, 65);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(366, 319);
+            this.treeView.TabIndex = 2;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 404);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(366, 34);
+            this.progressBar.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnBrowse;
+        private TextBox textBoxPath;
+        private TreeView treeView;
+        private ProgressBar progressBar;
+        private ToolTip toolTip;
+    }
+}
---
+++ Forms/MainForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageMetaEditor.Forms
+{
+    public partial class MainForm : Form
+    {
+        public static readonly List<string> ImageExtensions = new List<string> { ".JPG", ".JPEG", ".JPE", ".BMP", ".GIF", ".PNG" };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fbd = new OpenFileDialog()
+            {
+                ValidateNames = false,
+                CheckFileExists = false,
+                CheckPathExists = true,
+                FileName = "Folder Selection."
+            };
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+                textBoxPath.Text = Path.GetDirectoryName(fbd.FileName);
+        }
+
+        private void textBoxPath_TextChanged(object sender, EventArgs e)
+        {
+            // string[] entries = Directory.GetFileSystemEntries(textBoxPath.Text, "*", SearchOption.AllDirectories);
+            // Setting Inital Value of Progress Bar  
+            progressBar.Value = 0;
+            // Clear All Nodes if Already Exists  
+            treeView.Nodes.Clear();
+            toolTip.ShowAlways = true;
+            if (textBoxPath.Text != "" && Directory.Exists(textBoxPath.Text))
+                LoadDirectory(textBoxPath.Text);
+            else
+                MessageBox.Show("Select Directory!!");
+        }
+        public void LoadDirectory(string Dir)
+        {
+            DirectoryInfo di = new DirectoryInfo(Dir);
+            //Setting ProgressBar Maximum Value  
+            progressBar.Maximum = Directory.GetFiles(Dir, "*.*", SearchOption.AllDirectories).Length + Directory.GetDirectories(Dir, "**", SearchOption.AllDirectories).Length;
+            TreeNode tds = treeView.Nodes.Add(di.Name);
+            tds.Tag = di.FullName;
+            tds.StateImageIndex = 0;
+            LoadFiles(Dir, tds);
+            LoadSubDirectories(Dir, tds);
+        }
+        private void LoadSubDirectories(string dir, TreeNode td)
+        {
+            // Get all subdirectories  
+            string[] subdirectoryEntries = Directory.GetDirectories(dir);
+            // Loop through them to see if they have any other subdirectories  
+            foreach (string subdirectory in subdirectoryEntries)
+            {
+
+                DirectoryInfo di = new DirectoryInfo(subdirectory);
+                TreeNode tds = td.Nodes.Add(di.Name);
+                tds.StateImageIndex = 0;
+                tds.Tag = di.FullName;
+                LoadFiles(subdirectory, tds);
+                LoadSubDirectories(subdirectory, tds);
+                UpdateProgress();
+
+            }
+        }
+        private void LoadFiles(string dir, TreeNode tn)
+        {
+            string[] Files = Directory.GetFiles(dir, "*.*");
+
+            // Loop through them to see files  
+            foreach (string file in Files)
+            {
+                if (ImageExtensions.Contains(Path.GetExtension(file).ToUpperInvariant()))
+                {
+                    FileInfo fi = new FileInfo(file);
+                    TreeNode tns = tn.Nodes.Add(fi.Name);
+                    tns.Tag = fi.FullName;
+                    tns.StateImageIndex = 1;
+                    UpdateProgress();
+                }
+            }
+        }
+        private void UpdateProgress()
+        {
+            if (progressBar.Value < progressBar.Maximum)
+            {
+                progressBar.Value++;
+                int percent = (int)(((double)progressBar.Value / (double)progressBar.Maximum) * 100);
+                progressBar.CreateGraphics().DrawString(percent.ToString() + "%", new Font("Arial", (float)8.25, FontStyle.Regular), Brushes.Black, new PointF(progressBar.Width / 2 - 10, progressBar.Height / 2 - 7));
+
+                Application.DoEvents();
+            }
+        }
+    }
+}
